cb9bab348f2ff38f7edf60f5c35c6f6bcc1f1d9f
Chris Tiller <ctiller256@gmail.com>
2014-10-28 12:19:16 -0400

    Dhiraj edits

--- inventory_main.cpp
+++ inventory_main.cpp
@@ -1,12 +1,12 @@
 #include"inventory.h"
-#include"inventory.cpp"
 
-int main()
-{
-	cout << setprecision(2) << fixed << endl;  // set decimal to two decimal places
-	system("mode con cols=80 lines=43");  // resizing the console window
-	Inventory I;
-	I.menuDisplay();
-	system("cls");
-	return 0;
-}
+
+//int main()
+//{
+//	cout << setprecision(2) << fixed << endl;  // set decimal to two decimal places
+//	system("mode con cols=80 lines=43");  // resizing the console window
+//	Inventory I;
+//	I.menuDisplay();
+//	system("cls");
+//	return 0;
+//}
